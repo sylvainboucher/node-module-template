47a23d23c2c6c2904be3cc64693b420e9e48d7cf
Sylvain Boucher <sylvainboucher99@gmail.com>
2015-01-07 10:13:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 exports = module.exports = function module(options) {
   // get environment
-  var env = process.env.NODE_ENV || 'development'
+  var env = process.env.NODE_ENV || 'development';
 
   // get options
-  var opts = options || {}
-
-  // get log option
-  var log = opts.log === undefined
-    ? env !== 'test'
-    : opts.log
-
-  if (typeof log !== 'function' && typeof log !== 'boolean') {
-    throw new TypeError('option log must be function or boolean')
-  }
-
-  // default logging using console.error
-  if (log === true) {
-    log = function logerror(err, str) {
-      console.error(str)
-    }
-  }
+  var opts = options || {};
 
   return function module(err, req, res, next){
 
